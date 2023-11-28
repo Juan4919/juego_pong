1014498ab17c67146e95491a618c1476d6efc606
rolando <isabelino.rolando@gmail.com>
2023-11-28 21:00:20 +0100

    evento de teclado y uso de tasa de refresco

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import pygame as pg
-from figura_class import Pelota
+from figura_class import Pelota,Raqueta
 
 pg.init()
 
 pantalla_principal = pg.display.set_mode( (800,600) )
 pg.display.set_caption("Pong")
+#definir tasa de refresco en nuestro bucle de fotogramas, fps= fotograma por segundos
+tasa_refresco= pg.time.Clock()
 
 #creamos un objeto de la clase Pelota o instanciamos la clase pelota
 pelota = Pelota(400,300,(228, 231, 19),15)
 
+raqueta1 = Raqueta(10 ,300 )#raqueta izquierda
+raqueta2 = Raqueta(790,300 )#raqueta derecha
+
 game_over = True
 
 while game_over:
+    #obtener la tasa de refresco en milisegundos
+    valor_tasa = tasa_refresco.tick(300)#variable para controlar la velocidad entre fotogramas
+    #print(valor_tasa)
 
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = False
+
+
+    estado_teclado = pg.key.get_pressed()
+    #print("estado teclado: ",estado_teclado[pg.K_UP] )
+
+    if estado_teclado[pg.K_UP] == True:
+        raqueta1.pos_y -= 1
+
+    if estado_teclado[pg.K_DOWN] == True:
+        raqueta1.pos_y += 1    
 
     pantalla_principal.fill( ( 27, 149, 47 ) )
 
     pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), width=10)
 
     pelota.dibujar(pantalla_principal)
+    raqueta1.dibujar(pantalla_principal) 
+    raqueta2.dibujar(pantalla_principal)
 
     pg.display.flip()
 
